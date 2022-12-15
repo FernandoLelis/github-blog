827808c37b3fe75b7a65a5d6f62897adca9a774a
FernandoLelis <fernandolelisdarocha@gmail.com>
2022-12-15 15:11:52 -0300

    Componente profile finalizado

--- src/pages/Blog/componentes/Profile/indes.tsx
+++ src/pages/Blog/componentes/Profile/indes.tsx
@@ -1,5 +1,8 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ExternalLink } from "../../../../components/Header/ExternalLink";
 import { ProfileContainer, ProfileDetails, ProfilePicture } from "./styles";
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faBuilding, faUserGroup } from "@fortawesome/free-solid-svg-icons";
 
 export function Profile() {
     return (
@@ -12,6 +15,23 @@ export function Profile() {
 
                     <ExternalLink text="Github" href="#" />
                 </header>
+                <p>
+                    Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.
+                </p>
+                <ul>
+                    <li>
+                        <FontAwesomeIcon icon={faGithub} />
+                        FernandoLelis
+                    </li>
+                    <li>
+                        <FontAwesomeIcon icon={faBuilding} />
+                        Gobos do Brasil
+                    </li>
+                    <li>
+                        <FontAwesomeIcon icon={faUserGroup} />
+                        123 seguidores
+                    </li>
+                </ul>
             </ProfileDetails>
         </ProfileContainer>
     )
