e6b3e1764ec07ac8ff9b100019d9ad055f48d112
FernandoLelis <fernandolelisdarocha@gmail.com>
2022-12-15 21:12:18 -0300

    Componente SearchInput finalizado

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,9 +1,11 @@
 import { Profile } from "./componentes/Profile/indes";
+import { SearchInput } from "./componentes/SearchInput";
 
 export function Blog() {
     return (
         <>
             <Profile />
+            <SearchInput />
         </>
     )
 }
