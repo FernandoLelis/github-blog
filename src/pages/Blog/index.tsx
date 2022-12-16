1260b7beb216098fe85ba4c1c638f876f98d4def
FernandoLelis <fernandolelisdarocha@gmail.com>
2022-12-15 21:54:22 -0300

    Componente Post Finalizado

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,11 +1,23 @@
+import { Post } from "./componentes/Post";
 import { Profile } from "./componentes/Profile/indes";
 import { SearchInput } from "./componentes/SearchInput";
+import { PostListContainer } from "./styles";
 
 export function Blog() {
     return (
         <>
             <Profile />
             <SearchInput />
+            <PostListContainer>
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+            </PostListContainer>
         </>
     )
 }
