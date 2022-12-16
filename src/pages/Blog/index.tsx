b331d304c40b42a87a4721ff7bfd0ea4f47662e7
FernandoLelis <fernandolelisdarocha@gmail.com>
2022-12-16 11:24:01 -0300

    Importação de posts do Github comcluída

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,22 +1,56 @@
+import { useCallback, useEffect, useState } from "react";
+import { api } from "../../lib/axios";
 import { Post } from "./componentes/Post";
 import { Profile } from "./componentes/Profile/indes";
 import { SearchInput } from "./componentes/SearchInput";
 import { PostListContainer } from "./styles";
 
+const username = import.meta.env.VITE_GITHUB_USERNAME;
+const repoName = import.meta.env.VITE_GITHUB_REPONAME;
+
+export interface IPost {
+    title: string;
+    body: string;
+    created_at: string;
+    number: number;
+    html_url: string;
+    comments: number;
+    user: {
+        login: string;
+    }
+}
+
 export function Blog() {
+    const [posts, setPosts] = useState<IPost[]>([]);
+    const [isLoading, setIsloading] = useState(true)
+
+    const getPosts = useCallback(async (query: string = "") => {
+        try {
+            setIsloading(true);
+            const response = await api.get(
+                `/search/issues?q=${query}%20repo:${username}/${repoName}`
+            );
+                console.log(response.data);;
+                
+                setPosts(response.data.items);
+        } finally {
+            setIsloading(false);
+        }
+    }, [posts]);
+
+    useEffect(() => {
+        getPosts();
+    }, []);
+
     return (
         <>
             <Profile />
             <SearchInput />
             <PostListContainer>
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
+                {posts.map((post) => (
+                    <Post key={post.number} post={post} />
+                ))}
+               
             </PostListContainer>
         </>
     )
